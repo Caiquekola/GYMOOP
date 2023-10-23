23decad886d5ee2fc95391dea5b5aca95313b321
cRievers <131414483+cRievers@users.noreply.github.com>
2023-10-23 14:05:56 -0300

    Update Ficha.java

--- Ficha.java
+++ Ficha.java
@@ -1,44 +1,26 @@
 package poogym;
 
-import java.util.ArrayList;
-public class Ficha {
-    //Exercicios, séries do Exc, quantidade de realizacao,peso
-    String nomeFicha;
-    ArrayList<String> exercicio = new ArrayList<String>();
-    ArrayList<Integer> serieExc = new ArrayList<Integer>();
-    ArrayList<Integer> repeticao = new ArrayList<Integer>();
-    ArrayList<Float> peso = new ArrayList<Float>();
-    private int qntExerc=0;
-
+import java.util.List;
+public class Ficha extends Exercicio {
+    private List<Exercicio> exercicio;
     
-    public void adcExercicio(String exc) {
-        this.exercicio.add(exc);
-        this.setQntExer(getQntExer()+1);
-    }
-    public void adcRepeticao(int rep) {
-        this.repeticao.add(rep);
+    public void novo(String nome, int repet, int series, double peso, String dia){
+        Exercicio exe = new Exercicio(nome, repet, series, peso, dia);
+        exercicio.add(exe);
     }
     
-    public void adcSerieExc(int serie) {
-        this.serieExc.add(serie);
+    public void zerar(){
+        exercicio.clear();
     }
-    public void adcPeso(float peso) {
-        this.peso.add(peso);
-    }
-
-    public int getQntExer() {
-        return qntExerc;
-    }
-
-    public void setQntExer(int qntExer) {
-        this.qntExerc = qntExer;
-    }
-    public void fichaFinal(){
-        for (int i = 0; i < qntExerc; i++) {
-            System.out.println("Exercicio: "+exercicio.get(i)
-                    +" "+serieExc.get(i)+"X"+repeticao.get(i)+" "+peso.get(i)+"KG");
+    
+    public void listar(){
+        for (int index = 0; index < exercicio.size(); index++) {
+            System.out.println(index + ": ");
+            exercicio.get(index).info();
         }
     }
     
-    
+    public void apagar(int index){
+        exercicio.remove(index);
+    }
 }
