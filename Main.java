6f9d593475ca4ba177ace3fc0850b66af44b8631
cRievers <131414483+cRievers@users.noreply.github.com>
2023-10-26 10:41:06 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -67,27 +67,34 @@ public class Main {
         System.out.println("  3 - Voltar ao menu anterior");
         System.out.print("  >> ");
     }
-    
-    static void menuInfoCorpo(){
+
+    static void menuInfoCorpo() {
         logo();
         System.out.println("  1 - Indice de Massa Corporal");
         System.out.println("  2 - Taxa metabolica basal");
         System.out.println("  3 - Exibir informacoes gerais");
         System.out.println("  4 - Quantidade de agua diaria");
         System.out.println("  5 - Voltar ao menu anterior");
-        System.out.print  ("  >> ");
+        System.out.print("  >> ");
     }
 
     static void menuModExc() {
         logo();
         System.out.println("  1 - Modificar nome");
-        System.out.println("  2 - Modificar repeticao");
-        System.out.println("  3 - Modificar serie");
+        System.out.println("  2 - Modificar series e repeticoes: ");
+        System.out.println("  3 - Modificar dia da semana");
         System.out.println("  4 - Modificar peso");
-        System.out.println("  5 - Modificar dia");
-        System.out.println("  6 - Modificar tudo");
-        System.out.println("  7 - Voltar ao menu anterior");
+        System.out.println("  5 - Modificar tudo");
+        System.out.println("  6 - Voltar ao menu anterior");
         System.out.print("  >> ");
+    }
+
+    static int menuExc() {
+        claudiano.getClientes().get(id).getFicha().listar();
+        System.out.print("\nInforme o exercicio a ser modificado: ");
+        int index = scan.nextInt();
+        scan.nextLine();
+        return index;
     }
 
     public static void main(String[] args) {
@@ -185,33 +192,78 @@ public class Main {
                                 int serie = scan.nextInt();
                                 scan.nextLine();
                                 System.out.print("Peso: ");
-                                float peso = scan.nextFloat();
+                                double peso = scan.nextDouble();
                                 scan.nextLine();
                                 System.out.print("Dia da Semana: ");
                                 String dia = scan.next();
                                 scan.nextLine();
                                 claudiano.adcExc(claudiano.getClientes().get(id), nome, rep, serie, peso, dia);
                                 break;
-                                
+
                             case 3: //modificar exercicio
-                                id = menuId();
-                                menuModExc();
-                                int menu = scan.nextInt();
-                                scan.nextLine();
-                                switch(menu){
-                                    case 1:
-                                        claudiano.getClientes().get(id).getFicha().listar();
-                                        System.out.print("\nInforme o exercicio a ser modificado: ");
-                                        int index = scan.nextInt();
-                                        scan.nextLine();
-                                        System.out.print("\nNovo nome do exercicio: ");
-                                        nome = scan.nextLine();
-                                        claudiano.getClientes().get(id).getFicha().getExercicio().get(index).setNomeExe(nome);
-                                        break;
-                                        
-                                }
+                                int menu;
+                                do {
+                                    id = menuId();
+                                    menuModExc();
+                                    menu = scan.nextInt();
+                                    scan.nextLine();
+                                    switch (menu) {
+                                        case 1: //modificar nome do exercicio
+                                            int index = menuExc();
+                                            System.out.print("\nNovo nome do exercicio: ");
+                                            nome = scan.nextLine();
+                                            claudiano.getClientes().get(id).getFicha().getExercicio().get(index).setNomeExe(nome);
+                                            break;
+
+                                        case 2: //modificar serie X repeticao
+                                            index = menuExc();
+                                            System.out.print("\nNova serie: ");
+                                            serie = scan.nextInt();
+                                            scan.nextLine();
+                                            System.out.print("\nNova repeticao: ");
+                                            rep = scan.nextInt();
+                                            scan.nextLine();
+                                            claudiano.getClientes().get(id).getFicha().getExercicio().get(index).setSeries(serie);
+                                            claudiano.getClientes().get(id).getFicha().getExercicio().get(index).setRepet(rep);
+                                            break;
+
+                                        case 3: //modificar dia da semana
+                                            index = menuExc();
+                                            System.out.print("\nNovo dia da semana: ");
+                                            dia = scan.nextLine();
+                                            claudiano.getClientes().get(id).getFicha().getExercicio().get(index).setDiaSemana(dia);
+                                            break;
+
+                                        case 4: //modificar peso
+                                            index = menuExc();
+                                            System.out.print("\nNovo peso: ");
+                                            peso = scan.nextInt();
+                                            claudiano.getClientes().get(id).getFicha().getExercicio().get(index).setPeso(peso);
+                                            break;
+
+                                        case 5: //modificar tudo
+                                            index = menuExc();
+                                            System.out.print("Nome do exercicio: ");
+                                            nome = scan.nextLine();
+                                            System.out.print("Repeticoes: ");
+                                            rep = scan.nextInt();
+                                            scan.nextLine();
+                                            System.out.print("Series: ");
+                                            serie = scan.nextInt();
+                                            scan.nextLine();
+                                            System.out.print("Peso: ");
+                                            peso = scan.nextFloat();
+                                            scan.nextLine();
+                                            System.out.print("Dia da Semana: ");
+                                            dia = scan.next();
+                                            scan.nextLine();
+                                            Exercicio ex = new Exercicio(nome, rep, serie, peso, dia);
+                                            claudiano.getClientes().get(id).getFicha().getExercicio().set(index, ex);
+                                            break;
+                                    }
+                                } while (menu < 6);
                                 break;
-                                
+
                             case 4: //apagar exercicio
                                 id = menuId();
                                 claudiano.getClientes().get(id).getFicha().listar();
@@ -221,46 +273,46 @@ public class Main {
                                 scan.nextLine();
                                 claudiano.getClientes().get(id).getFicha().apagar(nExc);
                                 break;
-                                
+
                             case 5: //exibir ficha
                                 claudiano.exibirFicha(claudiano.getClientes().get(menuId()));
                                 break;
-                                
+
                             case 6: //exibir info pessoal
                                 id = menuId();
                                 System.out.println(claudiano.getClientes().get(id).toString());
                                 break;
-                            
+
                             case 7:
                                 int info;
-                                do{
+                                do {
                                     menuInfoCorpo();
                                     info = scan.nextInt();
                                     scan.nextLine();
                                     id = menuId();
-                                    switch(info){
+                                    switch (info) {
                                         case 1: //IMC
                                             claudiano.getClientes().get(id).getCorpoCliente().analisaImc();
                                             break;
-                                            
+
                                         case 2: //TMB
                                             double tmb = claudiano.getClientes().get(id).getCorpoCliente().tmb();
                                             System.out.println("Taxa metabólica basal: " + tmb + " cal");
                                             break;
-                                            
+
                                         case 3: //InfoGeral
                                             claudiano.getClientes().get(id).getCorpoCliente().infoGeral();
                                             break;
-                                            
+
                                         case 4: //Agua
                                             double agua = claudiano.getClientes().get(id).getCorpoCliente().agua();
                                             System.out.println("Quantidade de agua diaria: " + agua + " mL");
                                             break;
                                     }
-                                    
-                                }while(info != 5);
+
+                                } while (info != 5);
                                 break;
-                                
+
                         }
                     } while (resp != 8);
                 default:
